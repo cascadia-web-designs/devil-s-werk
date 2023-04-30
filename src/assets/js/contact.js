0e89900a16529cd7972a1a5bd82de51dd740eef3
Gengur Sv <meleearthur@gmail.com>
2023-04-29 23:29:55 -0700

    Fixed the contact page

--- src/assets/js/contact.js
+++ src/assets/js/contact.js
@@ -12,9 +12,9 @@ const Contact = () => {
         ${bodyInput}`;
 
         Email.send({
-            SecureToken: "30e56138-0473-4603-938d-d21d560cf827",
-            To: 'kyruken@gmail.com',
-            From: "myfodderemail0@gmail.com",
+            SecureToken: "1c236d5b-9cd5-47ea-baff-f712d2b98393",
+            To: 'devilswerk@gmail.com',
+            From: "cwdevilswerk@gmail.com",
             Subject: subjectInput,
             Body: emailBody
         })
